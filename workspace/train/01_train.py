551878021d0308d1e0c8990c5b5fda13bf680be8
mariohgu <mariof737@outlook.com>
2024-04-05 18:45:34 -0500

    agregando configuraciones para trabajar con pt

--- workspace/train/01_train.py
+++ workspace/train/01_train.py
@@ -3,11 +3,11 @@ from ultralytics import YOLO
 
 def main():
     # Load a model
-    model = YOLO("yolov8n.yaml")  # build a new model from scratch
+    model = YOLO("yolov8n.pt")  # build a new model from scratch
 
     # Use the model
     results = model.train(
-        data="00_config.yaml", epochs=1, batch=8
+        data="00_config.yaml", epochs=1, batch=32, workers=2, device=0
     )  # train the model, puedes usar tambien el parametro workers=1 para mejorar el rendimiento, patience=10 para early stopping, imgsz=640 para mejorar el rendimiento,
 
 
